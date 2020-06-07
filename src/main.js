fe57e431b4063e2ccbc03cbae2d65345fb258f84
Cristofer Sousa <cristofer.sousa@gmail.com>
2020-06-07 01:46:04 -0300

    feat: add loader to data api in template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Loader from './components/Loader.vue';
 
 Vue.config.productionTip = false;
+Vue.component('Loader', Loader);
 
 new Vue({
   router,
