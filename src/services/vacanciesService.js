c9ccc8d56ade156612fc55b695540e3ba20b2528
Cristofer Sousa <cristofer.sousa@gmail.com>
2020-06-10 00:05:24 -0300

    feat: changes url api to https

--- src/services/vacanciesService.js
+++ src/services/vacanciesService.js
@@ -1,6 +1,6 @@
 import api from '../helpers/api';
 
-const url = 'http://www.mocky.io/v2/5d6fb6b1310000f89166087b';
+const url = 'https://run.mocky.io/v3/12ddb9a7-ad24-493a-8f18-b2ffbd2fb9c9';
 
 export default async function getVacancies() {
   const { data: { vagas } } = await api.get(url);
